0739b50ca10d436631faf8827253df5b2e90ae52
Mr-Naim <59228895+Mr-Naim@users.noreply.github.com>
2023-08-23 14:14:03 +0600

    Update index.js

--- inventory-mern-backend/index.js
+++ inventory-mern-backend/index.js
@@ -1,5 +1,8 @@
-const app=require("./app");
-const PORT=5050;
-app.listen(PORT,function () {
-    console.log("App Running successfully")
-})
+const app = require("./app");
+
+// Use the dynamic port assigned by Vercel or default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`App Running successfully on port ${PORT}`);
+});
